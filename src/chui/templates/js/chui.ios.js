854ab3fd6cc3188d5e42686e994c4bc5e43eede7
Craig Presti <craig@seventhcap.com>
2013-05-16 15:01:27 -0700

    Added updated chui files for iOS and Android

--- src/chui/templates/js/chui.ios.js
+++ src/chui/templates/js/chui.ios.js
@@ -1104,28 +1104,27 @@ Version: 2.1.4
 			
 			UINavigateBack : function() {
 				var histLen = $.UINavigationHistory.length;
-				var parent = $.UINavigationHistory[histLen-1];
-				$.UINavigationHistory.pop();
-				histLen = $.UINavigationHistory.length;
-				$($.UINavigationHistory[histLen-1])
-				.css('visibility', 'visible');
-				$($.UINavigationHistory[histLen-1])
-				.attr('ui-navigation-status', 'current');
-				
-				$($.UINavigationHistory[histLen-1])
-				.removeAttr('aria-hidden');
-				$(parent).attr('ui-navigation-status', 'upcoming');
-				$(parent).attr('aria-hidden', 'true');
-				$(parent).css('visibility', 'hidden');
+				var currentView = "#" + $('view[ui-navigation-status=current]')
+					.attr('id');
+				$($.UINavigationHistory[histLen -1])
+					.css({'visibility':'visible'})
+					.attr('ui-navigation-status', 'current');
+				$($.UINavigationHistory[histLen-1]).removeAttr('aria-hidden');
+				$(currentView).attr('ui-navigation-status', 'upcoming');
+				$(currentView).attr('aria-hidden', 'true');
+				$(currentView).css('visibility', 'hidden');
 				 if ($.app.attr('ui-kind')==='navigation-with-one-navbar' && $.UINavigationHistory[histLen-1] === '#main') {
  					$('navbar > uibutton[ui-implements=back]', $.app).css({'display':'none'});
+ 				}
+ 				$.UISetHashOnUrl($.UINavigationHistory[$.UINavigationHistory.length-1]);
+ 				if ($.UINavigationHistory[$.UINavigationHistory.length-1] !== '#main') {
+ 					$.UINavigationHistory.pop();
  				}
 			},
 			
 			UINavigateBackToView : function ( viewID ) {
 				var historyIndex = $.UINavigationHistory.indexOf(viewID);
 				$.UINavigationHistory = $.UINavigationHistory.splice(historyIndex);
-				console.log($.UINavigationHistory);
 				var views = $('app').findAll('views');
 				$._each(views, function(idx, ctx) {
 					if ($(ctx).attr('ui-navigation-status' == 'current')) {
@@ -1140,33 +1139,47 @@ Version: 2.1.4
 			UINavigationListExits : false,
 
 		   UINavigationEvent : false,
+		   
+			UIOutputHashToUrl : null,
+			
+			UITrackHashNavigation : function ( url, delimeter ) {
+				url = url || true;
+				$.UIOutputHashToUrl = url;
+				$.UISetHashOnUrl($.UINavigationHistory[$.UINavigationHistory.length-1], delimeter);
+			},
 			
 			UINavigationList : function() {
 				var navigateList = function(node) {
-					var currentNavigatingView = '#main';
+					var currentNavigatingView = $(node).closest('view');
+					var currentViewID = '#' + $(currentNavigatingView).attr('id');
 					node = $(node);
 					node.attr('role','link');
 					var href = node.attr('href');
 					if (/^#/.test(href) == false) return;
 					try {
-						if ($.app.attr('ui-kind')==='navigation-with-one-navbar') {
-							$('app > navbar > uibutton[ui-implements=back]').css({'display': 'block'});
+						if ($.UIOutputHashToUrl) {
+							if ($(href).hasAttr('ui-uri')) {
+								$.UISetHashOnUrl($(href).attr('ui-uri'), '#');
+								console.log('#' + $(href).attr('ui-uri'));
+							} else {
+								$.UISetHashOnUrl(href);
+							}
 						}
 						$(node.attr('href')).attr('ui-navigation-status', 'current');
 						$(node.attr('href')).removeAttr('aria-hidden');
 						$(node.attr('href')).css('visibility', 'visible');
-						$($.UINavigationHistory[$.UINavigationHistory.length-1]).attr('ui-navigation-status', 'traversed');
-						$($.UINavigationHistory[$.UINavigationHistory.length-1]).attr('aria-hidden', 'true');
-						$($.UINavigationHistory[$.UINavigationHistory.length-1]).css('visibility', 'hidden');
+						$(currentViewID).attr('ui-navigation-status', 'traversed');
+						$(currentViewID).attr('aria-hidden', 'true');
+						$(currentViewID).css('visibility', 'hidden');
 						if ($('#main').attr('ui-navigation-status') !== 'traversed') {
 							$('#main').attr('ui-navigation-status', 'traversed');
 							$('#main').attr('aria-hidden', 'true');
 							$('#main').css('visibility', 'hidden');
 						}
 						
-						$.UINavigationHistory.push(href);
-						currentNavigatingView = node.closest('view');
-						
+						if (currentViewID != '#main') {
+							$.UINavigationHistory.push(currentViewID);
+						}
 						currentNavigatingView.on('webkitTransitionEnd', function(event) {
 							if (_jq) {
 								if (event.type === 'webkitTransitionEnd') {
@@ -1223,6 +1236,10 @@ Version: 2.1.4
 			},
 			
 			UINavigateToView : function(viewID) {
+				var currentView = $('view[ui-navigation-status=current]').attr('id');
+				if (currentView !== 'main') {
+					$.UINavigationHistory.push("#" + currentView);
+				}
 				$.UINavigationListExits = true;
 				var histLen = $.UINavigationHistory.length;
 				$($.UINavigationHistory[histLen-1]).attr('ui-navigation-status','traversed');
@@ -1231,19 +1248,46 @@ Version: 2.1.4
 				$(viewID).attr('ui-navigation-status','current');
 				$(viewID).removeAttr('aria-hidden');
 				$(viewID).css('visibility', 'visible');
-				if (viewID != '#main') {
-					$.UINavigationHistory.push(viewID);
-				}
 				if ($.app.attr('ui-kind') === 'navigation-with-one-navbar') {
 					try {
 						$('navbar uibutton[ui-implements=back]').css({'display':'block'});
 						$('navbar uibutton[ui-implements=backTo]').css({'display':'block'});
 					} catch(err) {}
 				}
+				if ($(viewID).hasAttr('ui-uri')) {
+					$.UISetHashOnUrl($(viewID).attr('ui-uri'));
+				} else {
+				   $.UISetHashOnUrl(viewID);
+				}
 			},
 			
 			UINavigateToNextView : function ( viewID ) {
 				return $.UINavigateToView(viewID);
+			},
+			
+			UISetHashOnUrl : function ( url, delimiter ) {
+				delimiter = delimiter || '#/';
+				if ($.UIOutputHashToUrl) {
+					var hash;
+					if (/^#/.test(url)) {
+						hash = delimiter + (url.split('#')[1]);
+					} else {
+						hash = delimiter + url;
+					}
+					window.history.replaceState('Object', 'Title', hash);
+				}
+			},
+			
+			UISetAppStateFromRoute : function ( route ) {
+				if (route) {
+					$.UIOutputHashToUrl = route;
+					// Code here:
+					
+				} else {
+					$.UIOutputHashToUrl = true;
+					// Code here:
+					
+				}
 			},
 		
 			resetApp : function ( hard ) {
@@ -1403,9 +1447,9 @@ Version: 2.1.4
 		
 		$.app.delegate('view','webkitTransitionEnd', function() {
 			if (!$('view[ui-navigation-status=current]')) {
-				$($.UINavigationHistory[$.UINavigationHistory.length-2])	 
+				$($.UINavigationHistory[$.UINavigationHistory.length-1])	 
 					.attr('ui-navigation-status', 'current');
-				$.UINavigationHistory.pop(); 
+				//$.UINavigationHistory.pop(); 
 			}	
 			$.UINavigationEvent = false;
 		});
@@ -1423,7 +1467,7 @@ Version: 2.1.4
 				if ($(node).attr('ui-implements') === 'back') {
 					if ($.UINavigationListExits) {
 						$.UINavigateBack();
-						$.UINavigationEvent = false;
+						//$.UINavigationEvent = false;
 					}
 				}
 			});
@@ -1437,7 +1481,7 @@ Version: 2.1.4
 				if ($(node).attr('ui-implements') === 'back') {
 					if ($.UINavigationListExits) {
 						$.UINavigateBack();
-						$.UINavigationEvent = false;
+						//$.UINavigationEvent = false;
 					}
 				}
 			});	
