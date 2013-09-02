aa523d5e44358b170049ca4b0a1677160edcc25c
Craig Presti <craig@seventhcap.com>
2013-09-02 11:03:04 -0700

    Upgraded ChUI to v3.0

--- src/chui/templates/index.php
+++ src/chui/templates/index.php
@@ -13,27 +13,25 @@
         {
             case DeviceType::Android:
             {
-                echo "<link rel=\"stylesheet\" href=\"".plugin_dir_url(__FILE__)."css/chui.android.min.css\">\n";
-                echo "<script type=\"text/javascript\" src=\"".plugin_dir_url(__FILE__)."js/iscroll.min.js\"></script>\n";
-	            echo "<script type=\"text/javascript\" src=\"".plugin_dir_url(__FILE__)."js/chocolatechip.js\"></script>\n";
-                echo "<script type=\"text/javascript\" src=\"".plugin_dir_url(__FILE__)."js/chui.android.js\"></script>\n";
+                echo "<link rel=\"stylesheet\" href=\"".plugin_dir_url(__FILE__)."css/chui.android-3.0.min.css\">\n";
+	            echo "<script type=\"text/javascript\" src=\"".plugin_dir_url(__FILE__)."js/chocolatechip-3.0.min.js\"></script>\n";
+                echo "<script type='text/javascript' src='".plugin_dir_url(__FILE__)."js/chui-3.0.min.js'></script>";
                 
                 break;
             }            
             case DeviceType::iOS:
             {
-                echo "<link rel='stylesheet' href='".plugin_dir_url(__FILE__)."css/chui.ios.min.css'>";
-                echo "<script type='text/javascript' src='".plugin_dir_url(__FILE__)."js/iscroll.min.js'></script>";
-                echo "<script type=\"text/javascript\" src=\"".plugin_dir_url(__FILE__)."js/chocolatechip.js\"></script>\n";
-                echo "<script type='text/javascript' src='".plugin_dir_url(__FILE__)."js/chui.ios.js'></script>";
+                echo "<link rel='stylesheet' href='".plugin_dir_url(__FILE__)."css/chui.ios-3.0.min.css'>";
+                echo "<script type=\"text/javascript\" src=\"".plugin_dir_url(__FILE__)."js/chocolatechip-3.0.min.js\"></script>\n";
+                echo "<script type='text/javascript' src='".plugin_dir_url(__FILE__)."js/chui-3.0.min.js'></script>";
                 
                 break;
             }
             case DeviceType::WindowsPhone:
             {
-	            echo "<link rel='stylesheet' href='".plugin_dir_url(__FILE__)."css/chui.wp.min.css'>";
-                echo "<script type=\"text/javascript\" src=\"".plugin_dir_url(__FILE__)."js/chocolatechip.js\"></script>\n";
-                echo "<script type='text/javascript' src='".plugin_dir_url(__FILE__)."js/chui.wp.js'></script>";
+	            echo "<link rel='stylesheet' href='".plugin_dir_url(__FILE__)."css/chui.win-3.0.min.css'>";
+                echo "<script type=\"text/javascript\" src=\"".plugin_dir_url(__FILE__)."js/chocolatechip-3.0.min.js\"></script>\n";
+                echo "<script type='text/javascript' src='".plugin_dir_url(__FILE__)."js/chui-3.0.min.js'></script>";
                 
                 break;
             }
